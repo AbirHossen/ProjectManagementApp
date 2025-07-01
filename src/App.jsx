a0d63e6c461e493ade3c73ca641653e2670c440a
Abir Hossen <abirhossen16@gmail.com>
2025-07-01 17:53:56 +0600

    update button input added modal, save button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
-import ProjectsSidebar from "./components/ProjectsSidebar";
-
+import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
+import { useState } from "react";
 function App() {
+  const [projectsState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+  function handleStartAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+  function handleAddProject(projectData) {
+    setProjectState((prevState) => {
+      const projectID = Math.random();
+      const newProject = {
+        ...projectData,
+        id: projectID,
+      };
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+
+  let content;
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
-    <main className="h-screen my-8">
-      <ProjectsSidebar />
+    <main className="h-screen my-8 flex gap-8 ">
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
+      {content}
     </main>
   );
 }
